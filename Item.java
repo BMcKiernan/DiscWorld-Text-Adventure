20081323a538af2613581fae0e7b813ee5398525
BMcKiernan <brimck89@gmail.com>
2016-11-11 02:47:28 -0500

    Item Class Added
    As of right now the item class only as a constructor that accepts a
    String name and a boolean for "takeable" and for "equipable."

---
+++ Item.java
@@ -0,0 +1,51 @@
+
+/**
+ * This is an item class and will help determine the actions that can be implemented with an item.
+ * 
+ * @author Brian McKiernan Nathan Paget
+ * @version 11/11/2016
+ */
+public class Item
+{
+   boolean equipable ;
+   boolean takeable;
+   String itemName;
+   
+   public Item(String itemName,boolean takeable, boolean equipable){
+       this.itemName = itemName;
+       this.takeable = takeable;
+       this.equipable = equipable;
+    }
+    
+    
+    /**
+     * take method returns a boolean about whether or not an item is takeable.
+     */
+   public boolean take(){
+       if(takeable == true)
+       {
+           return true;
+       }
+       else
+       {
+           return false;
+       }
+    }
+    
+    
+    /**
+     * equip Method returns a boolean about whether or not an item is equipable.
+     */
+   public boolean equip(){
+       
+       if(equipable == true)
+       {
+           return true;
+       }
+       else
+       {
+           return false;
+       }
+       
+   }
+}
